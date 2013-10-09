6b92fe744005c5603c10231801dcc4ed574c451e
edenisn <edenisn@gmail.com>
2013-10-09 21:04:59 +0400

    add url validation and bug fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   attr_accessible :published, :link, :body
 
   validates :link, :body, presence: true
+  validates :link, format: /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
 end
